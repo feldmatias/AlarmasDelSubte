446f99221c0d66fc072196b03ac2c0b489088572
feldmatias <feldmatias@gmail.com>
2020-01-30 21:17:44 -0300

    added request logger

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,17 @@ import {Db} from "./db/Db";
 import graphqlHTTP from "express-graphql";
 import {GraphQL} from "./graphql/GraphQL";
 import {registerCommandRoutes} from "./commands/routes";
+import morgan from "morgan";
 
 // Create a new express application instance
 const app: express.Application = express();
+
+app.use(express.json());
+morgan.token('body', (req, _res) => {
+    return JSON.stringify(req.body);
+});
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
+
 
 const bootstrap: () => Promise<void> = async () => {
     // Db
@@ -17,10 +25,7 @@ const bootstrap: () => Promise<void> = async () => {
     app.use('/graphql',
         graphqlHTTP({
             schema: schema,
-            graphiql: false,
-            customFormatErrorFn: (error) => {
-                return error.message;
-            }
+            graphiql: false
         }),
     );
 
